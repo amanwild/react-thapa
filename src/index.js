bd3968a8033e42c4a68061fb4c3a814267f74f86
Aman Sahu <amansahu1130@gmail.com>
2022-09-26 16:47:05 +0530

    react fragment 9

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <div>
-    <h1>Hello !!</h1>,<h1>Welcome to My world!!</h1>,
-  </div>
+    // React.Fragment is faster than div cause it dosn't craete extra div
+//   <React.Fragment>
+//     <h1>Hello !!</h1><h1>Welcome to My world!!</h1>
+//   </React.Fragment>
+//this is also syntax of react fragment
+  <>
+    <h1>Hello !!</h1><h1>Welcome to My world!!</h1>
+  </>
 );
