e0e571905fe58cc83378304462b49361701413fd
Aman Sahu <amansahu1130@gmail.com>
2022-09-26 17:17:00 +0530

    expression in JSX

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
+//Java script Code
+const w=" World"
+const b="Byebye "
 root.render(
-    // React.Fragment is faster than div cause it dosn't craete extra div
-//   <React.Fragment>
-//     <h1>Hello !!</h1><h1>Welcome to My world!!</h1>
-//   </React.Fragment>
-//this is also syntax of react fragment
+    
   <>
-    <h1>Hello !!</h1><h1>Welcome to My world!!</h1>
+  {/* JSX Code*/}
+    <h1>Hello !!</h1><h1>Welcome to My {w}!!</h1>
+    <h1>Hello !!</h1><h1>{10+10} is expression!!</h1>
+    <h1>Hello !!</h1><h1>{b} {w //Java Script
+    }!!</h1>
   </>
 );
