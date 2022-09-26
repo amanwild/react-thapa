87d22c411c579d55a89594618b6e216158f82586
Aman Sahu <amansahu1130@gmail.com>
2022-09-26 17:24:18 +0530

    Template literals

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-//Java script Code
-const w=" World"
-const b="Byebye "
+const w = " World";
+const b = "Byebye ";
 root.render(
-    
-  <>
-  {/* JSX Code*/}
-    <h1>Hello !!</h1><h1>Welcome to My {w}!!</h1>
-    <h1>Hello !!</h1><h1>{10+10} is expression!!</h1>
-    <h1>Hello !!</h1><h1>{b} {w //Java Script
-    }!!</h1>
+    <>
+    {/* Template literals */}
+    <h1>{`Welcome to My ${w}!!`}</h1>
+    <h1>{`${10 + 10} is expression!!`}</h1>
+    <h1>
+      {`${b} ${w} `}
+    </h1>
   </>
 );
