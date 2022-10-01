0776db45adfb7006c58a798f7675cb633704942a
Aman Sahu <amansahu1130@gmail.com>
2022-10-01 18:56:42 +0530

    useState in hook in react

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,22 @@
-import React from "react";
-import Header from "./Header";
-import Calc from "./Calc";
+import React, { useState } from "react";
+// import Header from "./Header";
+// import Calc from "./Calc";
 
-function App() {
+
+const App=() => {
+    const state = useState();
+    const [count,setCount]=useState(0);
+    
+    const incNum =()=>{
+    
+        console.log(count);
+        console.log("click");
+        setCount(count+1)
+    }
     return(
-
     <>
-        <Header />
-        <Calc />
+        <h1>{count}</h1>
+        <button onClick={incNum}>Click Me</button>
     </>);
 }
 export default App;
