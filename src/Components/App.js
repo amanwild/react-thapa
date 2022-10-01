f97fbbf1e02c985de4ffc2edfd58ad02881227f5
Aman Sahu <amansahu1130@gmail.com>
2022-10-01 23:22:32 +0530

    use of spread operator ,three dots (...)

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,38 +1,88 @@
 import React, { useState } from "react";
 const App = () => {
-  const state = useState();
-  const [nameF, setNameF] = useState("");
-  const [nameL, setNameL] = useState("");
-  const [resfname, setfname] = useState("");
-  const [reslname, setlname] = useState("");
+  //   const state = useState();
+  const [name, setName] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    phone: "",
+    degree: "",
+  });
 
-  const setnameF = (event) => {
-    setNameF(event.target.value);
+  const set = (event) => {
+    const { name, value } = event.target;
+
+    setName((preValue) => {
+      console.log(preValue);
+      console.log(name);
+      console.log(value);
+      return {
+        ...preValue,
+        [name]: value,
+      };
+    });
   };
-  const setnameL = (event) => {
-    setNameL(event.target.value);
-  };
+
   const result = (e) => {
     e.preventDefault();
-    setfname(nameF);
-    setlname(nameL);
+    console.log(e);
+
+    alert("form Submitted");
   };
   return (
     <>
       <div>
-      <form onSubmit={result}>
-        <h1> Hello {resfname} {reslname}</h1>
-        <input
-          type="text"
-          placeholder="First Name"
-          onChange={setnameF}
-        />
-        <input
-          type="text"
-          placeholder="Last Name"
-          onChange={setnameL}
-        />
-        <button type="submit" >Click me</button>
+        <form onSubmit={result}>
+          <h1>
+            {" "}
+            Hello {name.fname} {name.lname}
+          </h1>
+          <h1>
+            <p>{name.email}</p>
+            <p>{name.phone}</p>
+            <p>{name.degree}</p>
+          </h1>
+          <input
+            className="in"
+            type="text"
+            name="fname"
+            value={name.fname}
+            placeholder="First Name"
+            onChange={set}
+          />
+          <input
+            className="in"
+            type="text"
+            name="lname"
+            value={name.lname}
+            placeholder="Last Name"
+            onChange={set}
+          />
+          <input
+            className="in"
+            type="email"
+            name="email"
+            value={name.email}
+            placeholder="email"
+            onChange={set}
+          />
+          <input
+            className="in"
+            type="number"
+            name="phone"
+            value={name.phone}
+            placeholder="number"
+            onChange={set}
+          />
+          <input
+            className="in"
+            type="text"
+            name="degree"
+            value={name.degree}
+            placeholder="Last Name"
+            onChange={set}
+          />
+          <button type="submit">Click me</button>
         </form>
       </div>
     </>
