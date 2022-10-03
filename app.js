7944095b98fb80c54526472a03396a350f4465ec
Aman Sahu <amansahu1130@gmail.com>
2022-10-03 23:27:07 +0530

    react-router-dom Client side Routing

--- app.js
+++ app.js
@@ -4,18 +4,18 @@ const app=express();
 app.get('/',(req,res)=>{
     res.send(`This is home`)
 })
-app.get('/about',(req,res)=>{
-    res.send(`This is about`)
-})
-app.get('/contact',(req,res)=>{
-    res.send(`This is contact`)
-})
-app.get('/singin',(req,res)=>{
-    res.send(`This is singin`)
-})
-app.get('/singup',(req,res)=>{
-    res.send(`This is singup`)
-})
+// app.get('/about',(req,res)=>{
+//     res.send(`This is about`)
+// })
+// app.get('/contact',(req,res)=>{
+//     res.send(`This is contact`)
+// })
+// app.get('/singin',(req,res)=>{
+//     res.send(`This is singin`)
+// })
+// app.get('/singup',(req,res)=>{
+//     res.send(`This is singup`)
+// })
 var cors = require('cors')
 
 app.use(cors()) // Use this after the variable declaration
--- src/App.js
+++ src/App.js
@@ -1,49 +1,20 @@
-import axios from "axios";
-import React, { useEffect,useState } from "react";
+// npm i react-router-dom
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Error from "./Components/Error";
 const App = () => {
-  const [num, setNum] = useState("")
-  const [name, setName] = useState("")
-  const [moves, setmoves] = useState("")
-  
-  
-  useEffect(() => {
-    //use to avoid cors Ristriction or error
-    const config = {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
-      }
-    };
-
-    async function getData(){
-      const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}/`,config)
-      console.log("Name :",res.data.name)
-      console.log("Moves :",res.data.moves.length)
-      
-      setName(res.data.name) 
-      setmoves(res.data.moves.length)
-    }    ;
-    getData()
-    // these num defines ,for What in change to run useEffect
-  },[num]);
-
   return (
     <>
-      <div className="container">
-        <div>
-          <h1> You Choose <span style={{color :"red"}}>{num}</span></h1>
-          <h1> Name is  <span style={{color :"red"}}>{name}</span></h1>
-          <h1> Have <span style={{color :"red"}}>{moves}</span> Moves</h1>
-        </div>
-        <select value={num} onChange={(event)=>{
-          setNum(event.target.value);
-        }}>
-          <option value="" selected disabled hidden>Choose </option>
-          <option value="1">1</option>
-          <option value="25">25</option>
-          <option value="3">3</option>
-        </select>
-      </div>
+     <Router>
+        <Routes>
+        {/* exact is used for exact path  */}
+          <Route exact path="/" element= {<Home/>}/>
+          <Route exact path="/about" element={<About />} />
+          <Route path="*" element= {<Error/>} />
+        </Routes>
+      </Router>
     </>
   );
 };
